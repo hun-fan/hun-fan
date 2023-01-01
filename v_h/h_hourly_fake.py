05d95a20a1a4cf97fb91596649df31e46c1925a7
sergzhig <zse1@bk.ru>
2023-01-01 10:12:49 +0300

    Hfan2812

--- v_h/h_hourly_fake.py
+++ v_h/h_hourly_fake.py
@@ -61,8 +61,8 @@ def register(key):
         logging.warning(ip_text)
         logging.warning(city_text)
         sleep(5)
-        telegram.send_doc(caption=f'{name}{key}слот{start_time_dict[key]}H_hourly_fake{user}Проверка айпи{ip_text}-{city_text}',
-                          html=driver.page_source)
+        # telegram.send_doc(caption=f'{name}{key}слот{start_time_dict[key]}H_hourly_fake{user}Проверка айпи{ip_text}-{city_text}',
+        #                   html=driver.page_source)
 
         driver.delete_all_cookies()
         driver.get(sys.argv[4])
